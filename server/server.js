175909ead22c0a4ff27c2846462cf0da5474dfff
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2025-04-29 01:34:40 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-require("dotenv").config();
-
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
