f88f2101beddda4287cee08112aae32a7ce0752c
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2025-04-29 01:59:10 +0530

    bcrypt to bcryptjs

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const FormDataModel = require('../Model/UserSchema'); // Adjust the path as necessary
 const router = express.Router();
 
