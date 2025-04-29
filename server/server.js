9536d5da790dfff00468e2fe92be70adbfd79161
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2025-04-29 22:27:35 +0530

    MongoDb updation

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ console.log("MongoDB URI:", process.env.MONGO_URI);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
 })
     .then(() => {
         console.log('✅ Connected to MongoDB');
