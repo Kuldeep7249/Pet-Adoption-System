d2fc1e5f63c746830142c5d41e515d8498d5e7e3
Kuldeep7249 <152018228+Kuldeep7249@users.noreply.github.com>
2025-04-29 22:46:30 +0530

    mongoDb Update

--- server/server.js
+++ server/server.js
@@ -16,9 +16,7 @@ const app = express();
 console.log("MongoDB URI:", process.env.MONGO_URI);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('✅ Connected to MongoDB');
         const PORT = process.env.PORT || 4000;
